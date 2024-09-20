6c6576d855cb3a28b503859e83bc0a49f245b4a4
JIHyeongJun <wlgudwns6@g.shingu.ac.kr>
2024-09-20 10:19:44 +0900

    [Feat] Action , Event , Command 사용

---
+++ Assets/Script/SimpleCallback.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleCallback : MonoBehaviour
+{
+    private Action greetingAction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        greetingAction = SayHello;
+        PerformGreeting(greetingAction);
+    }
+
+    void SayHello()
+    {
+        Debug.Log("Hello, world");
+    }
+
+    void PerformGreeting(Action greetingFunc)
+    {
+        greetingFunc?.Invoke();
+    }
+}
